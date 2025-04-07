a9bf352ab3c892494d0a75320ae81121f889b752
Jeffreyna21 <jeffrey.navarrete@udla.edu.ec>
2025-04-07 09:08:04 -0500

    cambios realizados

---
+++ NavarreteClase02/Controllers/EquipoController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NavarreteClase02.Models;
+using NavarreteClase02.Repositories;
+
+namespace NavarreteClase02.Controllers
+{
+    public class EquipoController : Controller
+    {
+        public IActionResult View()
+        {
+            return View();
+        }
+        public ActionResult List()
+        {
+
+            EquipoRepository repository = new EquipoRepository();
+            var equipos = repository.DevuelveListadoEquipos();
+
+            equipos = equipos.OrderBy(item => item.PartidosGanados);
+            //equipos = equipos.Where(item => item.Nombre == "Liga de Quito");
+            return View(equipos);
+        }
+    }
+}
---
+++ NavarreteClase02/Repositories/EquipoRepository.cs
@@ -0,0 +1,34 @@
+﻿using NavarreteClase02.Models;
+
+namespace NavarreteClase02.Repositories
+{
+    public class EquipoRepository
+    {
+        public IEnumerable<Equipo> DevuelveListadoEquipos()
+        {
+            List<Equipo> equipos = new List<Equipo>();
+            Equipo ldu = new Equipo
+            {
+                /*datos*/
+                Id = 1,
+                Nombre = "Liga de Quito",
+                PartidosJugados = 10,
+                PartidosGanados = 5,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0
+            };
+            Equipo barcelona = new Equipo
+            {
+                Id = 1,
+                Nombre = "Liga de Quito",
+                PartidosJugados = 10,
+                PartidosGanados = 3,
+                PartidosEmpatados = 0,
+                PartidosPerdidos = 0
+            };
+            equipos.Add(ldu);
+            equipos.Add(barcelona);
+            return equipos;
+        }
+    }
+}
